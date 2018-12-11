afc13b0ecb0cd15f6597994f32f5234feb7d6d84
Tim Wang <timwangdev@gmail.com>
2018-12-11 17:42:31 +0800

    Add experiment iOS locale support

--- ios/RNGeocoder/RNGeocoder.m
+++ ios/RNGeocoder/RNGeocoder.m
@@ -8,15 +8,14 @@
 
 + (CLLocation *)CLLocation:(id)json
 {
-  json = [self NSDictionary:json];
-
-  double lat = [RCTConvert double:json[@"lat"]];
-  double lng = [RCTConvert double:json[@"lng"]];
-  return [[CLLocation alloc] initWithLatitude:lat longitude:lng];
+    json = [self NSDictionary:json];
+    
+    double lat = [RCTConvert double:json[@"lat"]];
+    double lng = [RCTConvert double:json[@"lng"]];
+    return [[CLLocation alloc] initWithLatitude:lat longitude:lng];
 }
 
 @end
-
 
 @implementation RNGeocoder
 
@@ -26,27 +25,32 @@ RCT_EXPORT_METHOD(geocodePosition:(CLLocation *)location
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-  if (!self.geocoder) {
-    self.geocoder = [[CLGeocoder alloc] init];
-  }
-
-  if (self.geocoder.geocoding) {
-    [self.geocoder cancelGeocode];
-  }
-
-  [self.geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
-
-    if (error) {
-      if (placemarks.count == 0) {
-          return reject(@"NOT_FOUND", @"geocodePosition failed", error);
-      }
-
-      return reject(@"ERROR", @"geocodePosition failed", error);
+    if (!self.geocoder) {
+        self.geocoder = [[CLGeocoder alloc] init];
     }
 
-    resolve([self placemarksToDictionary:placemarks]);
-
-  }];
+    if (self.geocoder.geocoding) {
+        [self.geocoder cancelGeocode];
+    }
+    
+    void (^handler)(NSArray<CLPlacemark *> *placemarks, NSError *error)= ^{
+        if (error) {
+            if (placemarks.count == 0) {
+                return reject(@"NOT_FOUND", @"geocodePosition failed", error);
+            }
+            
+            return reject(@"ERROR", @"geocodePosition failed", error);
+        }
+        resolve([self placemarksToDictionary:placemarks]);
+    };
+    
+    if (@available(iOS 11.0, *)) {
+        [self.geocoder reverseGeocodeLocation:location
+                              preferredLocale:nil
+                            completionHandler:handler];
+    } else {
+        [self.geocoder reverseGeocodeLocation:location completionHandler:handler];
+    }
 }
 
 RCT_EXPORT_METHOD(geocodeAddress:(NSString *)address
@@ -76,45 +80,45 @@ RCT_EXPORT_METHOD(geocodeAddress:(NSString *)address
 }
 
 - (NSArray *)placemarksToDictionary:(NSArray *)placemarks {
-
-  NSMutableArray *results = [[NSMutableArray alloc] init];
-
-  for (int i = 0; i < placemarks.count; i++) {
-    CLPlacemark* placemark = [placemarks objectAtIndex:i];
-
-    NSString *name = nil;
-
-    if (![placemark.name isEqualToString:placemark.locality] &&
-        ![placemark.name isEqualToString:placemark.thoroughfare] &&
-        ![placemark.name isEqualToString:placemark.subThoroughfare])
-    {
-        name = placemark.name;
+    
+    NSMutableArray *results = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < placemarks.count; i++) {
+        CLPlacemark* placemark = [placemarks objectAtIndex:i];
+        
+        NSString *name = nil;
+        
+        if (![placemark.name isEqualToString:placemark.locality] &&
+            ![placemark.name isEqualToString:placemark.thoroughfare] &&
+            ![placemark.name isEqualToString:placemark.subThoroughfare])
+        {
+            name = placemark.name;
+        }
+        
+        NSArray *lines = placemark.addressDictionary[@"FormattedAddressLines"];
+        
+        NSDictionary *result = @{
+            @"feature": name ?: [NSNull null],
+            @"position": @{
+                 @"lat": [NSNumber numberWithDouble:placemark.location.coordinate.latitude],
+                 @"lng": [NSNumber numberWithDouble:placemark.location.coordinate.longitude],
+                 },
+            @"country": placemark.country ?: [NSNull null],
+            @"countryCode": placemark.ISOcountryCode ?: [NSNull null],
+            @"locality": placemark.locality ?: [NSNull null],
+            @"subLocality": placemark.subLocality ?: [NSNull null],
+            @"streetName": placemark.thoroughfare ?: [NSNull null],
+            @"streetNumber": placemark.subThoroughfare ?: [NSNull null],
+            @"postalCode": placemark.postalCode ?: [NSNull null],
+            @"adminArea": placemark.administrativeArea ?: [NSNull null],
+            @"subAdminArea": placemark.subAdministrativeArea ?: [NSNull null],
+            @"formattedAddress": [lines componentsJoinedByString:@", "] ?: [NSNull null]
+        };
+        
+        [results addObject:result];
     }
-
-    NSArray *lines = placemark.addressDictionary[@"FormattedAddressLines"];
-
-    NSDictionary *result = @{
-     @"feature": name ?: [NSNull null],
-     @"position": @{
-         @"lat": [NSNumber numberWithDouble:placemark.location.coordinate.latitude],
-         @"lng": [NSNumber numberWithDouble:placemark.location.coordinate.longitude],
-         },
-     @"country": placemark.country ?: [NSNull null],
-     @"countryCode": placemark.ISOcountryCode ?: [NSNull null],
-     @"locality": placemark.locality ?: [NSNull null],
-     @"subLocality": placemark.subLocality ?: [NSNull null],
-     @"streetName": placemark.thoroughfare ?: [NSNull null],
-     @"streetNumber": placemark.subThoroughfare ?: [NSNull null],
-     @"postalCode": placemark.postalCode ?: [NSNull null],
-     @"adminArea": placemark.administrativeArea ?: [NSNull null],
-     @"subAdminArea": placemark.subAdministrativeArea ?: [NSNull null],
-     @"formattedAddress": [lines componentsJoinedByString:@", "] ?: [NSNull null]
-   };
-
-    [results addObject:result];
-  }
-
-  return results;
+    
+    return results;
 
 }
 
