17b2a45156593544364c1ba55e59182d40820a3b
nonth <nonthz@gmail.com>
2019-08-17 22:59:12 +0700

    Added language parameter to googleApi.js

--- js/googleApi.js
+++ js/googleApi.js
@@ -58,12 +58,12 @@ function format(raw) {
 }
 
 export default {
-  geocodePosition(apiKey, position) {
+  geocodePosition(apiKey, position, language = 'en') {
     if (!apiKey || !position || (!position.lat && position.lat!==0) || (!position.lng && position.lng!==0)) {
       return Promise.reject(new Error("invalid apiKey / position"));
     }
 
-    return this.geocodeRequest(`${googleUrl}?key=${apiKey}&latlng=${position.lat},${position.lng}`);
+    return this.geocodeRequest(`${googleUrl}?key=${apiKey}&latlng=${position.lat},${position.lng}&language=${language}`);
   },
 
   geocodeAddress(apiKey, address) {
