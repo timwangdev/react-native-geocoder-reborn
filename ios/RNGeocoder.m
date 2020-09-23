e4fe8541ea9e764d80cdc9cf7dbd8e225b6f85df
Yao Wang <tim.wangyao@gmail.com>
2020-09-23 02:30:00 +0100

    fix: update error messages

--- ios/RNGeocoder.m
+++ ios/RNGeocoder.m
@@ -41,7 +41,7 @@ RCT_EXPORT_METHOD(geocodePosition:(CLLocation *)location
                 return reject(@"EMPTY_RESULT", @"Geocoder returned an empty list.", error);
             }
 
-            return reject(@"NATIVE_ERROR", @"reverseGeocodeLocation failed.", error);
+            return reject(@"NATIVE_ERROR", @"geocodePosition failed.", error);
         }
         resolve([self placemarksToDictionary:placemarks maxResult:maxResult]);
     };
@@ -74,7 +74,7 @@ RCT_EXPORT_METHOD(geocodeAddress:(NSString *)address
             if (placemarks.count == 0) {
               return reject(@"NOT_FOUND", @"Geocoder returned an empty list.", error);
             }
-            return reject(@"NATIVE_ERROR", @"geocodeAddressString failed.", error);
+            return reject(@"NATIVE_ERROR", @"geocodeAddress failed.", error);
         }
         resolve([self placemarksToDictionary:placemarks maxResult:maxResult]);
     };
@@ -111,7 +111,7 @@ RCT_EXPORT_METHOD(geocodeAddressInRegion:(NSString *)address
             if (placemarks.count == 0) {
               return reject(@"NOT_FOUND", @"Geocoder returned an empty list.", error);
             }
-            return reject(@"NATIVE_ERROR", @"geocodeAddressString failed.", error);
+            return reject(@"NATIVE_ERROR", @"geocodeAddressInRegion failed.", error);
         }
         resolve([self placemarksToDictionary:placemarks maxResult:maxResult]);
     };
